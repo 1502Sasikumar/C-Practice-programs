df238d179f03f3836f7208f8d4878cca8359e7d9
SASIKUMAR S <160245202+1502Sasikumar@users.noreply.github.com>
2025-09-25 10:24:35 +0530

    Reverse the array Elements.cpp

---
+++ Reverse the array Elements.cpp
@@ -0,0 +1,31 @@
+//Reverse the array Elements.
+#include<iostream>
+using namespace std;
+
+void ReverseArray(int arr[],int n)
+{
+    int temp=0,i,j;
+    for(i=0,j=n-1; i!=j; i++,j--){
+        temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]= temp;
+    }
+    
+}
+int main()
+{
+    int n;
+    cout<<"Enter Size:"<<endl;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+    ReverseArray(arr,n);
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+        
+    cout<<endl;
+
+    
+    return 0;
+}
